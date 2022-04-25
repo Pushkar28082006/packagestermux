7212e6d457ed58cef69eea961873ca5aa929a257
Pushkar28082006 <104379373+Pushkar28082006@users.noreply.github.com>
2022-04-25 22:24:45 +0530

    Add files via upload

---
+++ packages.sh
@@ -0,0 +1,26 @@
+#!/data/data/com.termux/files/usr/bash
+apt update -y                      #update in progress
+apt install python -y           #installation......
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install unzip -y
+apt install tor -y
+apt install wget -y
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+echo " If You Connected To Internet Then  "
+echo " YOUR PACKAGES IS INSTALLED SUCCESSFULLY "
+exit
